1e0be69e6f50506ad54847c4c849963f93208465
mahsajafari3855 <mahsa.jafari3855@gmail.com>
2022-04-09 21:23:30 +0430

    add loader

--- src/component/landing.js
+++ src/component/landing.js
@@ -1,19 +1,31 @@
-import React ,{useEffect} from 'react';
+import React ,{useEffect,useState} from 'react';
 import { getCoin } from '../services/api';
+import Spiner from './spiner';
 
 const Landing = () => {
+     const[coins,setCoin]=useState([]);
    useEffect(()=>{
        const fetchApi=async()=>{
            const data=await getCoin();
            console.log(data);
+           setCoin(data)
 
        }
        fetchApi()
 
    },[])
     return (
-        <div>
-        </div>
+        <>
+            <input type="text" placeholder='Search'/>
+            {coins.length?
+            <div>
+                            {coins.map(coin=> <p key={coins.id}> {coin.name}</p>)}
+                             
+
+            </div>
+            : <Spiner/>}
+            
+        </>
     );
 };
 
---
+++ src/component/spiner.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Spiner = () => {
+    return (
+        <div>
+<h3> .....Loading.....</h3>        </div>
+    );
+};
+
+export default Spiner;
