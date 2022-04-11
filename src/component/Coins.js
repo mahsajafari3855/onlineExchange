b6797c41f5dafb438a273cf4b0a5f6e25cf78586
mahsajafari3855 <mahsa.jafari3855@gmail.com>
2022-04-11 20:56:48 +0430

    sort table

--- src/component/Coins.js
+++ src/component/Coins.js
@@ -8,12 +8,15 @@ const Coins = ({ image, name, symbol, price, marketPrice, priceChange }) => {
       <span className={styles.name}> {name}</span>
       <span className={styles.symbol}> {symbol}</span>
       <span className={styles.currentPrice}> {price}</span>
-      <span className={styles.marketCap}> {marketPrice}</span>
       <span
-        className={priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange}
+        className={
+          priceChange > 0 ? styles.greenPriceChange : styles.redPriceChange
+        }
       >
         {priceChange.toFixed(2)}
       </span>
+
+      <span className={styles.marketCap}> {marketPrice}</span>
     </div>
   );
 };
